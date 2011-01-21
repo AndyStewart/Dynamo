b16705eda6ba1d4134cd894426460db39db8c35e
AndyStewart <Andy.Stewart@i-nnovate.net>
2011-01-21 13:35:39 +0000

    Basic save and load functionality

---
+++ ActiveRecord.Specs/RepositorySpecs.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using Machine.Specifications;
+
+namespace ActiveRecord.Specs
+{
+    public class When_loading_back_contents_of_simple_table_into_entity
+    {
+        Because of = () => contact = new Repository().FindBySql<Contact>("Select * from Contact");
+        It should_return_values = () => ((string)contact[0].FirstName).ShouldEqual("Andy");
+        It should_populate_full_name = () => ((string)contact[0].FullName).ShouldEqual("Mr Andy");
+
+        private static dynamic contact;
+    }
+
+    public class When_loading_back_contents_of_generic_sql
+    {
+        Because of = () => contact = new Repository().FindBySql("Select Contact.Id, Contact.FirstName, Company.Name from Contact inner join Company on (Company.Id=Contact.Company_Id)");
+        It should_return_values_from_contact_table = () => ((string)contact[0].FirstName).ShouldEqual("Andy");
+        It should_return_values_from_company_table = () => ((string)contact[0].Name).ShouldEqual("Company Name");
+
+        private static dynamic contact;
+    }
+
+    public class When_saving_new_entity
+    {
+        Establish context = () => contact = new Contact();
+
+        private Because of = () =>
+                                 {
+                                     contact.FirstName = "First Name";
+                                     repository.Save(contact);
+                                     dbContact = repository.FindBySql<Contact>("Select * from Contact where Id=" + contact.Id);
+                                 };
+
+        It Should_save_contacts_first_name = () => ((string)dbContact[0].FirstName).ShouldEqual("First Name");
+
+        private static dynamic contact;
+        private static Repository repository = new Repository();
+        private static IList<dynamic> dbContact;
+    }
+
+    public class Contact : Entity
+    {
+        public string FullName
+        {
+            get
+            {
+                return "Mr " + self.FirstName;
+            }
+        }
+    }
+}
---
+++ ActiveRecord/Repository.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Dynamic;
+
+namespace ActiveRecord
+{
+
+    public class Repository
+    {
+        private string ConnectionString = @"Data Source=.\sql2005;Initial Catalog=BobsDb;Integrated Security=True";
+
+        public IList<dynamic> FindBySql(string sqlString)
+        {
+            return FindBySql<Entity>(sqlString);
+        }
+
+        public IList<dynamic> FindBySql<T>(string sqlString)
+        {
+            var sqlConnection = new SqlConnection(ConnectionString);
+            sqlConnection.Open();
+            var command = new SqlCommand(sqlString, sqlConnection);
+
+            using (var reader = command.ExecuteReader(CommandBehavior.CloseConnection))
+            {
+                var results = new List<dynamic>();
+                if (reader == null)
+                    return results;
+
+                while (reader.Read())
+                {
+                    var entity = (IEntity)Activator.CreateInstance<T>();
+                    entity.Populate(reader);
+                    results.Add(entity);
+                }
+                reader.Close();
+                return results;
+            }
+        }
+
+        public void Save(Entity entity)
+        {
+            var sqlConnection = new SqlConnection(ConnectionString);
+            sqlConnection.Open();
+
+            var entityType = entity.GetType();
+            var sqlString = "INSERT INTO " + entityType.Name;
+
+            var columnNameString = "";
+            var valueString = "";
+            foreach (var property in entity.Properties)
+            {
+                columnNameString += property.Key + ",";
+                valueString += valueEncode(property.Value) + ",";
+            }
+
+            columnNameString = columnNameString.TrimEnd(',');
+            valueString = valueString.TrimEnd(',');
+
+            sqlString = sqlString + "(" + columnNameString + ") VALUES (" + valueString + "); SELECT @@IDENTITY";
+
+            var command = new SqlCommand(sqlString, sqlConnection);
+            var result = command.ExecuteScalar();
+            ((dynamic) entity).Id = result;
+        }
+
+        private string valueEncode(object value)
+        {
+            var valueType = value.GetType();
+
+            if (valueType == typeof(string))
+                return "'" + value + "'";
+
+            return value.ToString();
+        }
+    }
+
+    public interface IEntity
+    {
+        void Populate(IDataReader reader);
+    }
+
+    public class Entity : DynamicObject, IEntity
+    {
+        protected dynamic self;
+
+        public Entity()
+        {
+            Properties = new Dictionary<string, object>();
+            self = this;
+        }
+
+        public Dictionary<string, object> Properties { get; set; }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            result = Properties[binder.Name];
+            return true;
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            Properties[binder.Name] = value;
+            return true;
+        }
+
+        public void Populate(IDataReader reader)
+        {
+            for (var i = 0; i < reader.FieldCount; i++)
+            {
+                Properties.Add(reader.GetName(i), reader[i]);
+            }
+        }
+    }
+}
